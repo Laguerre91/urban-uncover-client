10c07a1f3f7ff8ecd5c06eaecf96bc17b585264d
Jeronimo Olea <jerooleag2004@gmail.com>
2024-02-15 16:14:24 +0100

    Pages started/Navlink

---
+++ src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,10 @@
+
+function HomePage(){
+    return(
+        <div>
+            ADIOS
+        </div>
+    )
+}
+
+export default HomePage
---
+++ src/Pages/Madrid/Madrid.jsx
@@ -0,0 +1,12 @@
+
+
+function Madrid() {
+
+    return (
+        <h1>
+            HOLA
+        </h1>
+    )
+}
+
+export default Madrid
