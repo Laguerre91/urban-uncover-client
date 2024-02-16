fb591f7678cfe80529399c4bbcd831c7a5d9cee8
Lara Aguerre <aguerre.lara@gmail.com>
2024-02-16 10:00:07 +0100

    rendering cities and add routes page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 import './App.css'
+import "bootstrap/dist/css/bootstrap.css";
+
+import AppRoutes from './routes/AppRoutes.jsx'
 import Navbar from './components/Navbar/Navbar.jsx'
-import HomePage from './pages_/HomePage/HomePage.jsx'
-import CityDetails from './pages_/CityDetails/CityDetails.jsx'
 import Footer from './components/Footer/Footer.jsx'
 
-import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
-      <Routes>
-        <Route path='/cities/:cityId' element={<CityDetails />} />
-        <Route path='/' element={<HomePage />} />
-      </Routes>
+      <AppRoutes />
       <Footer />
     </div>
   )
--- src/pages_/CitiesList/CitiesList.jsx
+++ src/pages_/CitiesList/CitiesList.jsx
@@ -1,7 +1,35 @@
+import axios, { Axios } from "axios"
+import { useEffect, useState } from "react"
+
+const API_URL_BASE = "http://localhost:5005/cities"
+
 const CitiesList = () => {
+
+    const [cities, setCities] = useState([])
+
+    useEffect(() => loadAllCities(), [])
+
+    const loadAllCities = () => {
+
+        axios
+            .get(`${API_URL_BASE}`)
+            .then(({ data }) => setCities(data))
+            .catch(err => console.log(err))
+    }
 
     return (
         <section className="CitiesList">
+            <h2>Discover all Cities</h2>
+
+            {
+                cities.map(elm => {
+                    return (
+                        <div key={elm.id}>
+                            <h1>{elm.name}</h1>
+                        </div>
+                    )
+                })
+            }
 
         </section>
     )
