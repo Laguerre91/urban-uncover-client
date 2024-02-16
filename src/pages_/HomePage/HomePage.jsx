5c8bf809df4d1ede38eaa04a8f2701765fd65216
Lara Aguerre <aguerre.lara@gmail.com>
2024-02-16 10:42:15 +0100

    caroussel added

--- src/pages_/HomePage/HomePage.jsx
+++ src/pages_/HomePage/HomePage.jsx
@@ -1,8 +1,27 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
 
-function HomePage(){
-    return(
+import ControlledCarousel from "../../components/ControlledCarousel/ControlledCarousel"
+
+const API_URL_BASE = "http://localhost:5005/cities"
+
+function HomePage() {
+
+    const [cities, setCities] = useState([])
+
+    useEffect(() => getAllCities(), [])
+
+    const getAllCities = () => {
+
+        axios
+            .get(`${API_URL_BASE}`)
+            .then(({ data }) => setCities(data))
+            .catch(err => console.log(err))
+    }
+
+    return (
         <div>
-            ADIOS
+            <ControlledCarousel />
         </div>
     )
 }
