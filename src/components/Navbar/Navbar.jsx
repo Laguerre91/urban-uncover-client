2b85b7b5582e9c465cfb5007b63e02d2bc806ca3
Jeronimo Olea <jerooleag2004@gmail.com>
2024-02-22 13:49:14 +0100

    search bar semi-functional

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,35 +5,54 @@ import axios from 'axios';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import AddActivityForm from '../AddActivityForm/AddActivityForm';
-import './Navbar.css'
-import letteringLogo from './../../assets/images/letteringLogo.png'
+import { Form, FormControl } from 'react-bootstrap';
+import './Navbar.css';
+import letteringLogo from './../../assets/images/letteringLogo.png';
 
-
-
-const API_URL_BASE = "http://localhost:5005/cities"
-
-
+const API_URL_BASE = "http://localhost:5005/cities";
+const JSON_SERVER_BASE_URL = 'http://localhost:5005';
 
 const NavBar = () => {
-
-  const [cities, setCities] = useState([])
+  const [budget, setBudget] = useState('');
+  const [cities, setCities] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
 
   useEffect(() => {
-    fetchCities()
-  }, [])
-
+    fetchCities();
+  }, []);
 
   const fetchCities = () => {
     axios
       .get(API_URL_BASE)
       .then(response => {
-        setCities(response.data)
+        setCities(response.data);
       }).catch(err => {
-        console.log(err)
-      })
+        console.log(err);
+      });
+  };
+  
+  const handleBudgetChange = (e) => {
+    e.preventDefault()
+    const value = e.target.value;
+    setBudget(value);
+    if (value) {
+      axios
+        .get(`${JSON_SERVER_BASE_URL}/activities?activitySpecs.price_lt${value}`)
+        .then(response => {
+          setSearchResults(response.data);
+        }).catch(err => {
+          console.log(err);
+        });
+    } else {
+      setSearchResults([]);
+    }
   }
 
+  const handleKeyDown = e => {
+    if (e.key === 'Enter') {
+      e.preventDefault()
+    }
+  }
 
   return (
     <Navbar expand='md' fixed='top' className='Navbar NavbarSpacing justify-content-between'>
@@ -43,24 +62,41 @@ const NavBar = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-
           <NavLink className='nav-link' to={'/about'}>About Us</NavLink>
           <NavDropdown title="Cities" id="cities-dropdown">
-
             {cities.map(city => (
               <NavDropdown.Item key={city.id} as={'div'} className='dropdown-items'>
                 <NavLink className="cities-link" to={`/cities/${city.id}`} cityid={city.id}>
                   {city.name}
-
                 </NavLink>
               </NavDropdown.Item>
             ))}
-
           </NavDropdown>
         </Nav>
+        <Form className="d-flex position-relative">
+          <FormControl
+            type="search"
+            placeholder="Enter maximum price"
+            className="mr-2"
+            value={budget}
+            onChange={handleBudgetChange}
+            onKeyDown={handleKeyDown}
+          />
+          {searchResults.length > 0 && (
+            <ul className="dropdown-menu position-absolute" style={{ display: 'block', position: 'absolute' }}>
+              {searchResults.map(result => (
+                <li key={result.id} className="dropdown-items">
+                  <NavLink to={`/cities/activities/${result.id}`} className="cities-link">
+                    {result.name} - ${result.price}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          )}
+        </Form>
       </Navbar.Collapse>
     </Navbar>
   );
-}
+};
 
 export default NavBar;
